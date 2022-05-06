09a78bbf5a67c3ad65c79d32c614f58299b1cc55
yakulbeansMacLaptop <yakulbeans@gmail.com>
2022-05-06 15:46:21 -0600

    App prints out Artist Prayer all at once

---
+++ ArtistPrayer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+char *Prayer;
+
+int main()
+{
+	printf("%s", Prayer);
+	return 0;
+}
+
+char *Prayer = "O Great Muse\n"
+			   "Gift us with your presence\n"
+			   "And we shall commit to the work of showing up\n"
+			   "Let us handle the quantity, and you handle the quality\n"
+			   "Help us love ourselves so we can love others\n"
+			   "Help us grow so we can help others grow\n"
+			   "Please alert us when we are on the wrong path\n"
+			   "By showing us interesting new directions to follow\n"
+			   "Let us not get wrapped up in the end product, but the whole process\n"
+			   "Help us love the present and work out of it not for an imaginary future\n"
+			   "But to make art for its own sake and its own beauty\n"
+			   "Grant us the courage to try, and the confidence for having done so\n"
+			   "Help us unite with other creators so we can encourage each other\n"
+			   "If we are depressed, help us cling to what is beautiful in the moment\n"
+			   "And help nudge us towards contentment even in difficult times\n"
+			   "We promise to offer our work to you consistently\n"
+			   "And we will listen to where our hearts tell us to go\n"
+			   "We dedicate our creations to you and they do not belong to us\n"
+			   "For we were only gifted the idea through the ether\n"
+			   "And we are merely forging it and returning it to the universe\n"
+			   "Allow us to realize we all deserve love\n"
+			   "And that we are all the same divine light\n"
+			   "We cannot be alone when we realize this\n"
+			   "We create in honor of each other's divinity\n"
+		       "Please accept our gifts as they are only made possible through your grace\n";
+
+				
+			  
